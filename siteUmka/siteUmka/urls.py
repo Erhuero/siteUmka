23de82af3338e1fe2b49fa80fff2975200e74500
Constantin CHTANKO <constantin.chtanko@etu.formation-aftec.com>
2019-11-07 20:35:36 +0100

    Créer une application Django initiale

--- siteUmka/siteUmka/urls.py
+++ siteUmka/siteUmka/urls.py
@@ -3,6 +3,7 @@ Definition of urls for siteUmka.
 """
 
 from django.conf.urls import include, url
+import UmkaPage.views
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -10,8 +11,10 @@ from django.conf.urls import include, url
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', siteUmka.views.home, name='home'),
-    # url(r'^siteUmka/', include('siteUmka.siteUmka.urls')),
+    url(r'^$', UmkaPage.views.index, name='index'), #le $ est le routage pour la racine du site
+    #url(r'^siteUmka/', include('siteUmka.siteUmka.urls')),
+
+    url(r'^$home', UmkaPage.views.index, name='home'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
