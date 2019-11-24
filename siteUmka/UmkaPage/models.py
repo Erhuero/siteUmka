bd85ac9f20d157833e5bf83f7ac4f91fdd753af7
Constantin CHTANKO <constantin.chtanko@etu.formation-aftec.com>
2019-11-24 21:59:07 +0100

    Models

--- siteUmka/UmkaPage/models.py
+++ siteUmka/UmkaPage/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
+#probleme de psycopg2 : installer via Outils -> Python -> Environnements Python -> vue d'ensemble -> ouvrir dans powerShell
+#-> pip3 install psycopg2
+#Remettre dans l'ordre les tables pour que les clés étrangères soient reconnues
+
 class Personne(models.Model):
     idPersonne = models.AutoField(primary_key=True)
 
+class Planning(models.Model):
+    refPlanning = models.AutoField(primary_key=True)
+    stockagePlanning = models.CharField(max_length=500)
+
 class Administrateur(models.Model):
     idPersonne= models.IntegerField(primary_key=True)
-    idAdministrateur=models.IntegerField(primary_key=True)
 
     nomAdministrateur= models.CharField(max_length =50)
     prenomAdministrateur=models.CharField(max_length =50)
@@ -15,13 +22,11 @@ class Administrateur(models.Model):
 
     refPlanning=models.ForeignKey(Planning, on_delete=models.CASCADE)
 
-class Planning(models.Model):
-    refPlanning = models.AutoField(primary_key=True)
-    stockagePlanning = models.CharField(max_length=500)
 
-class Modifier(models.Model):
-    idEvenement=models.ForeignKey(Calendrier, on_delete=models.CASCADE)
-    refEvenement=models.ForeignKey(Planning, on_delete=models.CASCADE)
+class TypeEvenement(models.Model):
+    idTypeEvenement=models.AutoField(primary_key=True)
+
+    libelleEvenement=models.CharField(max_length=50)
 
 class Calendrier(models.Model):
     idEvenement=models.AutoField(primary_key=True)
@@ -30,23 +35,24 @@ class Calendrier(models.Model):
     moisEvenement=models.DateField()
     anneeEvenement=models.DateField()
 
-    idTypeEvenement=models.ForeignKey()
+    idTypeEvenement=models.ForeignKey(TypeEvenement, on_delete=models.CASCADE)
 
-class TypeEvenement(models.Model):
-    idTypeEvenement=models.AutoField(primary_key=True)
+class Modifier(models.Model):
+    idEvenement=models.ForeignKey(Calendrier, on_delete=models.CASCADE)
+    refEvenement=models.ForeignKey(Planning, on_delete=models.CASCADE)
 
-    libelleEvenement=CharField(max_length=50)
-
-class Editer(models.Model):
-    refArticle=models.ForeignKey(Article, on_delete=models.CASCADE)
-    idPersonne=models.ForeignKey(Administrateur, on_delete=models.CASCADE)
-    idAdministrateur=models.ForeignKey(Administrateur, on_delete=models.CASCADE)
 
 class Article(models.Model):
     refArticle=models.AutoField(primary_key=True)
 
     nomArticle= models.CharField(max_length=50)
-    contenuArticle= models.CharField(maxlength=500)
+    contenuArticle= models.CharField(max_length=500)
+
+class Editer(models.Model):
+    refArticle=models.ForeignKey(Article, on_delete=models.CASCADE)
+    idPersonne=models.ForeignKey(Administrateur, on_delete=models.CASCADE)
+
+
 
 class Utilisateur(models.Model):
     idPersonne=models.IntegerField(primary_key=True)
@@ -56,23 +62,26 @@ class Utilisateur(models.Model):
     loginUtilisateur=models.CharField(max_length=50)
     motDePasseUtilisateur=models.CharField(max_length=50)
 
-class Telecharger(models.Model):
-    refDocument=models.ForeignKey(Document, on_delete=models.CASCADE)
-    idPersonne=models.ForeignKey(Personne, on_delete=models.CASCADE)
-    idUtilisateur=models.ForeignKey(Administrateur, on_delete=models.CASCADE)
+class TypeDocument(models.Model):
+    typeDocument=models.AutoField(primary_key=True)
 
+    libelleDocument=models.CharField(max_length=50)
+    
 class Document(models.Model):
     refDocument=models.AutoField(primary_key=True)
 
     nomDocument=models.CharField(max_length=50)
     tailleDocument=models.CharField(max_length=50)
 
-    typeDocument==models.ForeignKey(TypeDocument, on_delete=models.CASCADE)
+    typeDocument=models.ForeignKey(TypeDocument, on_delete=models.CASCADE)
 
-class TypeDocument(models.Model):
-    typeDocument=models.AutoField(primary_key=True)
+class Telecharger(models.Model):
+    refDocument=models.ForeignKey(Document, on_delete=models.CASCADE)
+    idPersonne=models.ForeignKey(Personne, on_delete=models.CASCADE)
+    idUtilisateur=models.ForeignKey(Administrateur, on_delete=models.CASCADE)
 
-    libelleDocument=models.CharField(max_length=50)
+
+
 
 
 
--- siteUmka/siteUmka/settings.py
+++ siteUmka/siteUmka/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'siteUmka.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+         'NAME': 'umka', #preciser un folder vide dans pgAdmin4
+        'USER': 'postgres',
+        'PASSWORD': 'ok',
+        'HOST': '127.0.0.1',
+        'PORT': '5433',
     }
 }
 
